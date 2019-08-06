7c7c8e8e813bd95f0ff6f42f8d12ecc71795e70b
Johan Nilsson <yoohahn@gmail.com>
2019-08-06 08:53:05 +0200

    Update 1.scratch-script.sh

--- ubuntu/1.scratch-script.sh
+++ ubuntu/1.scratch-script.sh
@@ -13,7 +13,7 @@ mkdir -p $HOME/git
 mkdir -p $HOME/.bin
 
 ## APPS AND THEMES
-sudo apt-get install -y curl git wget terminator net-tools kdiff3 htop arc-theme gnome-tweak-tool numix-gtk-theme numix-icon-theme chrome-gnome-shell
+sudo apt-get install -y curl git wget terminator net-tools kdiff3 htop arc-theme gnome-tweak-tool numix-gtk-theme numix-icon-theme chrome-gnome-shell lm-sensors
 
 ## Yubikey
 sudo wget -O /etc/udev/rules.d/70-u2f.rules https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules
