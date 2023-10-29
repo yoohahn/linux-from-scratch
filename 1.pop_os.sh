9c0cb10f9dc7e2745041b1eb53f3276d6d2ff9e7
Johan Nilsson <johan@pop-os.localdomain>
2023-10-29 11:24:36 +0100

    thorium browser

--- 1.pop_os.sh
+++ 1.pop_os.sh
@@ -39,6 +39,12 @@ curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/ala
 ## Yubikey
 sudo wget -O /etc/udev/rules.d/70-u2f.rules https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules
 
+## Thorium Browser
+wget https://dl.thorium.rocks/debian/dists/stable/thorium.list
+sudo mv thorium.list /etc/apt/sources.list.d/
+sudo apt update
+sudo apt install thorium-browser
+
 ## BRAVE   
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
