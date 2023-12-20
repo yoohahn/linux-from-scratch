96df2e1de86c0157c7dfefa709d9ff8de9e4a0db
Johan Nilsson <yoohahn@gmail.com>
2023-12-20 19:41:36 +0100

    Update 1.pop_os.sh

--- 1.pop_os.sh
+++ 1.pop_os.sh
@@ -57,6 +57,12 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 sudo apt update -y
 sudo apt install -y brave-browser
 
+## FLoorp
+curl -fsSL https://ppa.ablaze.one/KEY.gpg | sudo gpg --dearmor -o /usr/share/keyrings/Floorp.gpg
+sudo curl -sS --compressed -o /etc/apt/sources.list.d/Floorp.list 'https://ppa.ablaze.one/Floorp.list'
+sudo apt update
+sudo apt install floorp
+
 ## DOCKER
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
